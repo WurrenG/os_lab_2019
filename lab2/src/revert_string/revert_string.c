d55aa490859b5aaca4e5a4be5caa33aa449b16cd
Replit user <>
2023-12-21 00:21:44 +0000

    выполнена вторая лабораторная

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,13 @@
 #include "revert_string.h"
+#include "string.h"
+#include "stdio.h"
 
 void RevertString(char *str)
 {
-	// your code here
+	for (int i = 0; i < strlen(str)/2; i++){
+    char buff = str[i];
+    str[i] = str[strlen(str) - i - 1];
+    str[strlen(str) - i - 1] = buff;
+  }
 }
 
